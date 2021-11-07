5cec5d9608763844881ae672bb8e0d73aefcf29b
Ellie <jssoyeon@gmail.com>
2021-11-07 18:57:28 +0900

    edit board

--- test/src/main/java/com/myproject/myapp/service/BoardUpdateService.java
+++ test/src/main/java/com/myproject/myapp/service/BoardUpdateService.java
@@ -1,7 +1,36 @@
 package com.myproject.myapp.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myproject.myapp.dao.Dao;
+import com.myproject.myapp.domain.Board;
+import com.myproject.myapp.domain.BoardRequest;
+
+@Service
 public class BoardUpdateService {
 
+	private Dao dao;
 	
+	@Autowired
+	SqlSessionTemplate template;
 	
+	public int editBoard(BoardRequest boardRequest, Board board) {
+		
+		int resultCnt=0;
+		Board editBoard= board;
+		
+		editBoard.setTitle(boardRequest.getTitle());
+		editBoard.setContent(boardRequest.getContent());
+		editBoard.setWriter(boardRequest.getWriter());
+		editBoard.setPassword(boardRequest.getPassword());
+		
+		dao = template.getMapper(Dao.class);
+		resultCnt=dao.updateBoard(editBoard);
+		
+		return resultCnt;
+		
+		
+	}
 }
